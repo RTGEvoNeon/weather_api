bfcdcf790c9139df466c1575baf3565e4cf0fdc0
ilya <rtgevoneon@icloud.com>
2024-05-20 13:10:35 +0300

    Добавил файл для тестирования асинхронности

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import fastapi
 import uvicorn
+
 api = fastapi.FastAPI()
 
+@api.get('/')
+def index():
+    return fastapi.Response(content='Ты не туда идешь!')
+
 @api.get('/api/calculate')
-def calculate(x:int, y:int):
+def calculate(x: int, y: int):
     value = x + y
+    if value < 3:
+        return fastapi.Response(content='У нас великая малютка!', status_code=400, media_type='application/json')
+    elif value > 10:
+        return fastapi.responses.JSONResponse(content={"error": "Я боюсь темноты"}, status_code=400)
     return {
         'result': value
     }
-
 
 uvicorn.run(api, port=8000, host='127.0.0.1')
